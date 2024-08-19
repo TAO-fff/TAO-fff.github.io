221ddd21033129dc5bc88300ed23858d1e3e7bb5
TAO-fff <134177125+TAO-fff@users.noreply.github.com>
2024-08-20 00:04:25 +0900

    Add files via upload

---
+++ js/header_footer.js
@@ -0,0 +1,85 @@
+
+// header-nav 【hoverしたもの以外、薄く表示】
+$(document).ready(function () {
+    //$(document).ready →ウェブページの準備ができたら何かをする合図
+    $(".header__nav li").hover(
+      function () {
+        //header__nav liにhoverをしたら
+        $(this).siblings().css("opacity", "0.2");
+        /* this→現在の要素。 siblings→hその要素の兄弟要素。
+          現在の要素の兄弟要素(全て)のopacityを0.3に設定する */
+      },
+      function () {
+        $(".header__nav li").css(
+          "opacity",
+          "1",
+        ); /* hoverが外れたときに全てのli要素のopacityを元に戻します */
+      }
+    );
+  });
+
+
+
+
+
+
+// ham 【btn動き】
+$(function () {
+    $(".sp__menu__btn").on("click", function () {
+      $(".sp__menu__btn").toggleClass("open");
+      $(".sp__header__nav").fadeToggle();
+    });
+  
+    //hum click後閉じる
+    $(".sp__nav__list li").on("click", function () {
+      //header nav list liをclickしたら
+      $(".sp__menu__btn").removeClass("open");
+      //sp menu btn のopenクラスを消す。
+      $(".sp__header__nav").fadeOut();
+      //header nav フェードアウトしてもらう
+    });
+  });
+  
+
+
+
+// footer
+// site-map ボタン
+const targets = document.querySelectorAll('.animation-target');
+
+targets.forEach(target => {
+  const btn_hover = gsap.timeline({
+    paused: true
+  })
+  .to(target, {
+    background: "#f5f5f5",
+  })
+  .to(target.querySelectorAll(".footer-btn-text"), {
+    yPercent: -100,
+  }, "<");
+
+  target.addEventListener('mouseenter', () => btn_hover.play());
+  target.addEventListener('mouseleave', () => btn_hover.reverse());
+});
+
+// sns-icon ボタン
+document.addEventListener("DOMContentLoaded", function() {
+  const btns = document.querySelectorAll(".icon-btn");
+
+  btns.forEach(button => {
+    button.addEventListener("mousemove", (e) => {
+      const buttonRect = button.getBoundingClientRect();
+      const offsetX = e.clientX - buttonRect.left;
+      const offsetY = e.clientY - buttonRect.top;
+      const x = (offsetX - buttonRect.width / 2) * 1.1;
+      const y = (offsetY - buttonRect.height / 2) * 1.1;
+
+      button.style.transform = `translate(${x}px, ${y}px) scale(1.03)`;
+    });
+
+    button.addEventListener("mouseleave", () => {
+      button.style.transform = "translate(0, 0) scale(1)";
+      button.style.transition = "transform 0.2s ease-out";
+    });
+  });
+});
